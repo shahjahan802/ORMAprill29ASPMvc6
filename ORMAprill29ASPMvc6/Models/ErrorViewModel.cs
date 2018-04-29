1bcc27a827bad9d4dddc47b3a44580c3180e3bf3
shahjahan <shahjahan7868@outlook.com>
2018-04-29 12:29:44 -0700

    Add project files.

---
+++ ORMAprill29ASPMvc6/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ORMAprill29ASPMvc6.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
